f39f36667b294a6d5b02eda12a7a52b50420ab06
daushvilitea-cyber <daushvilitea@gmail.com>
2026-04-20 14:06:57 +0200

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+const Navbar = ({ list }) => {
+  return (
+    <div>
+      {list.map((item) => (
+        <div key={item.id}>{item.title}</div>
+      ))}
+    </div>
+  );
+};
+
+export default Navbar;
